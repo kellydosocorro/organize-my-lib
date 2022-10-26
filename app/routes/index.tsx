7363fc333f63af6cb3539d11b4164a13c4a755d7
Kelly Costa <kellydosocorro@gmail.com>
2022-10-26 00:01:00 -0300

    FIX: redirect when user is not logged in

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,19 @@
 import { Link, Form } from "@remix-run/react";
-
+import type { LoaderArgs } from "@remix-run/node";
+import { useUser } from "~/utils";
+import { getUserId } from "~/session.server";
 import { redirect } from "@remix-run/node";
 
-import { useOptionalUser } from "~/utils";
+export async function loader({ request }: LoaderArgs) {
+  const userId = await getUserId(request);
+  if (!userId) {
+    throw redirect("/login");
+  }
+  return userId;
+}
 
 export default function Index() {
-  const user = useOptionalUser();
+  const user = useUser();
   if (user) {
     return (
       <div className="flex h-full min-h-screen flex-col">
@@ -73,50 +81,5 @@ export default function Index() {
         </main>
       </div>
     )
-  } else {
-    redirect('/login')
   }
-  /* return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div className="hero min-h-screen" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
-        <div className="hero-overlay bg-opacity-50"></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">
-              Organize my Lib!
-            </h1>
-            <p className="mb-5">A free option to save your readings.</p>
-
-            <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-              {user ? (
-                <Link
-                  to="/panel"
-                  className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
-                >
-                  View Notes for {user.email}
-                </Link>
-              ) : (
-                <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-                  <Link
-                    to="/join"
-                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
-                  >
-                    Sign up
-                  </Link>
-                  <Link
-                    to="/login"
-                    className="flex items-center justify-center rounded-md bg-blue-500 px-4 py-3 font-medium text-white hover:bg-blue-400"
-                  >
-                    Log In
-                  </Link>
-                </div>
-              )}
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </main>
-
-  ); */
 }
